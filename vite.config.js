7f775c91e98783c70be86a64b11340aab3cb5f43
Tackyjarl <wallaceandrew990@gmail.com>
2025-02-09 16:15:20 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/final-project",
+  base: "/DnD-Compendium-and-Character-Creator",
   plugins: [react()],
 });
